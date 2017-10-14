5f5c4131a14e36b04d3578e1c91cc65d5b767ec2
Tushar Pokle <tushar.pokle@gmail.com>
2017-10-15 10:26:45 +1100

    Promises work. But code in need of refactoring and tests! Cyclomatic complexity is on the increase!

--- ioX.js
+++ ioX.js
@@ -10,11 +10,13 @@
   type Actions = { [Action]: (IO) => any }
 */
 
-const readEventFromQueue = then => ({ io: 'read-event-from-queue', then });
-const writeToDisk = (value, then) => ({ io: 'write-to-disk', value, then });
-const log = (m, then) => ({ io: 'log', m, then });
+const Promise = require('bluebird');
 
-function someProcess()/*:IO*/ {
+const readEventFromQueue = then          => ({ io       : 'read-event-from-queue', then });
+const writeToDisk        = (value, then) => ({ io       : 'write-to-disk', value, then });
+const log                = (m, then=()   => 1) => ({ io : 'log', m, then });
+
+function someProcess() /*:IO*/ {
   return readEventFromQueue(event => {
     if (event === 'END') {
       return 'The meaning of life is 41.99999999...';
@@ -26,30 +28,43 @@ function someProcess()/*:IO*/ {
   });
 }
 
-function makeSimulator()/*:Actions*/ {
+function makeSimulator() /*:Actions*/ {
   const events = ['first event', 'second event', 'END'];
   return {
     'read-event-from-queue': io => events.shift(),
-    'write-to-disk': io => console.log('writing to disk',io),
+    'write-to-disk': io =>
+      new Promise((resolve, reject) => setTimeout(x => resolve('👍'), 2000)),
     log: io => console.log('logging', io)
   };
 }
 
 function isIO(value /*: IO | mixed */) {
-  return Boolean(!!value && typeof value === 'object' && typeof value.io === 'string');
+  return Boolean(
+    !!value &&
+      typeof value === 'object' &&
+      typeof value.io === 'string' &&
+      typeof value.then === 'function'
+  );
 }
 
-function run(actions/*:Actions*/, value /*:IO*/) {
+function run(actions /*:Actions*/, value /*:IO*/) {
   while (true) {
+    if (value instanceof Promise) return value.then(v => run(actions, v));
     if (!isIO(value)) return value;
 
     const io /*:IO*/ = value;
     const ioFunc = actions[io.io];
     if (ioFunc == null) throw new Error('Unknown io operation' + value.io);
 
-    value = io.then(ioFunc(io));
+    const next = ioFunc(io);
+    if (next instanceof Promise)
+      return next.then(io.then).then(v => run(actions, v));
+    else value = io.then();
   }
 }
 
-const result = run(makeSimulator(), someProcess());
-console.log(result);
+// run(makeSimulator(), writeToDisk('x', r=>r)).then(console.log)
+//run(makeSimulator(), log('⧴', _=>writeToDisk('x', r=>r))).then(console.log)
+run(makeSimulator(), log('⧴', _ => writeToDisk('x', log))).then(r => console.log('r:', r))
+
+//console.log(result);
