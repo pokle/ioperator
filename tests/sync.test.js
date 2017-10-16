e72825e635a76d109efb80fc11bad0c81a97d63b
Tushar Pokle <tushar.pokle@gmail.com>
2017-10-16 14:00:30 +1100

    cleaned up tests somewhat

--- tests/sync.test.js
+++ tests/sync.test.js
@@ -2,22 +2,10 @@ const jsc = require('jsverify');
 const iox = require('..');
 
 describe('Synchronous IO', () => {
-  it('Should fail when IO action is not known', () => {
-    expect(() =>
-      iox.run({}, { io: 'some-unknown-action', then: () => 1 })
-    ).toThrowError(/Unknown io action: some-unknown-action/);
-  });
-
-  it('Should not fail when no then is provided for io action', () => {
-    const actions = { scrub: io => 'lub-a-dub-dub' };
-    expect(iox.run(actions, { io: 'scrub' })).toEqual('lub-a-dub-dub');
-  });
-
   it('should return sync values as is', () => {
     jsc.assert(
-      jsc.forall(
-        'falsy | bool | number | string | array | json',
-        v => iox.run({}, v) === v
+      jsc.forall('falsy | bool | number | string | array | json', v =>
+        iox.run({}, v).then(result => result === v)
       )
     );
   });
@@ -35,19 +23,26 @@ describe('Synchronous IO', () => {
       return inc(value, dec);
     };
 
-    expect(iox.run(actions, someProcess(123))).toEqual(123);
+    expect.assertions(1);
+    return expect(iox.run(actions, someProcess(123))).resolves.toEqual(123);
   });
 
   test('sync io errors', () => {
     const actions = {
-      'nop': io => true,
+      nop: io => true,
       'will-fail': io => {
         throw new Error("I just can't help failing");
       }
     };
 
-    const someProcess = () => ({ io: 'nop', then: io => ({ io: 'will-fail' })});
+    const someProcess = () => ({
+      io: 'nop',
+      then: io => ({ io: 'will-fail' })
+    });
 
-    expect(() => iox.run(actions, someProcess())).toThrowError("I just can't help failing")
+    expect.assertions(1);
+    return expect(iox.run(actions, someProcess())).rejects.toMatchObject(
+      new Error("I just can't help failing")
+    );
   });
 });
