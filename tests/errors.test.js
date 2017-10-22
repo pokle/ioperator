132623b4aa07898ce639f288c3e302cf05183c47
Tushar Pokle <tushar.pokle@gmail.com>
2017-10-22 21:38:30 +1100

    Unbabel (#8)
    * Recursive non es6 version
    
    * rename
    
    * back to commonjs
    
    * benchmark
    
    * whee
    
    * t

--- tests/errors.test.js
+++ tests/errors.test.js
@@ -1,8 +1,15 @@
-import { run } from '../src';
+const { run } = require('..');
 
 test('is an error to call run() with a non io value', () => {
   expect.assertions(1);
-  return expect(run({}, 'non-io-value')).rejects.toMatchObject(
-    new Error('Not an IO: non-io-value')
+  return expect(() => run({}, 'non-io-value')).toThrow(
+    new Error('ioperator.run called with a non io-action: non-io-value')
+  );
+});
+
+test('is an error to call run() without actions', () => {
+  expect.assertions(1);
+  return expect(() => run(undefined, 'non-io-value')).toThrow(
+    new Error('ioperator.run called without actions')
   );
 });
--- tests/sync.test.js
+++ tests/sync.test.js
@@ -1,4 +1,4 @@
-import { run } from '../src'
+const { run } = require('..');
 
 describe('Synchronous IO', () => {
 
