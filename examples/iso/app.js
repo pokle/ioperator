e0443fe9a4df4174ad25b818ef59a7a10f1ca04f
Tushar Pokle <tushar.pokle@gmail.com>
2017-10-22 15:32:03 +1100

    Reformatted

--- examples/iso/app.js
+++ examples/iso/app.js
@@ -1,28 +1,19 @@
 // A pure isomorphic application
-const React = require('react')
+const React = require('react');
 const routes = routes => ({ io: 'routes', routes });
 
+const Home = () => (
+  <div>
+    {' '}
+    Hey, you should go <a href="/hey">here</a>{' '}
+  </div>
+);
+
 const Hello = ({ name }) => <h1>Hello {name}</h1>;
+const Hey = () => <Hello name="there" />;
 
 function app() /*:IO*/ {
-  return routes([
-    {
-      path: '/',
-      then() {
-        return (
-          <div>
-            Hey, you should go <a href="/hey">here</a>
-          </div>
-        );
-      }
-    },
-    {
-      path: '/hey',
-      then() {
-        return <Hello name="there" />;
-      }
-    }
-  ]);
+  return routes([{ path: '/', then: Home }, { path: '/hey', then: Hey }]);
 }
 
 module.exports = app;
